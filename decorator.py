f54518e425c0a53bd8fa8dd5fe5046e86d53d338
szhmery <szhmery@gmail.com>
2018-05-03 08:55:09 +0800

    add decorator

--- decorator.py
+++ decorator.py
@@ -1,3 +1,5 @@
+# -*-coding:utf-8-*-
+import logging
 def use_logging(func):
 
     def wrapper():
@@ -10,6 +12,98 @@ def foo():
 
 foo = use_logging(foo)  # 因为装饰器 use_logging(foo) 返回的时函数对象 wrapper，这条语句相当于  foo = wrapper
 foo()        
-main(){
-	foo()
-}
+
+
+
+def hi(name="yasoob"):
+    def greet():
+        return "now you are in the greet() function"
+
+    def welcome():
+        return "now you are in the welcome() function"
+
+    if name == "yasoob":
+        return greet
+    else:
+        return welcome
+
+a = hi(name="teset")
+print(a)
+#outputs: <function greet at 0x7f2143c01500>
+
+#上面清晰地展示了`a`现在指向到hi()函数中的greet()函数
+#现在试试这个
+
+print(a())
+#outputs: now you are in the greet() function
+
+def hi():
+    return "hi yasoob!"
+
+def doSomethingBeforeHi(func):
+    print("I am doing some boring work before executing hi()")
+    print(func())
+
+doSomethingBeforeHi(hi)
+
+
+def a_new_decorator(a_func):
+
+    def wrapTheFunction():
+        print("I am doing some boring work before executing a_func()")
+
+        a_func()
+
+        print("I am doing some boring work after executing a_func()")
+
+    return wrapTheFunction
+
+def a_function_requiring_decoration():
+    print("I am the function which needs some decoration to remove my foul smell")
+
+a_function_requiring_decoration()
+#outputs: "I am the function which needs some decoration to remove my foul smell"
+
+a_function_requiring_decoration = a_new_decorator(a_function_requiring_decoration)
+#now a_function_requiring_decoration is wrapped by wrapTheFunction()
+
+a_function_requiring_decoration()
+#outputs:I am doing some boring work before executing a_func()
+#        I am the function which needs some decoration to remove my foul smell
+#        I am doing some boring work after executing a_func()
+
+print "use @"
+@a_new_decorator
+def a_function_requiring_decoration():
+    """Hey you! Decorate me!"""
+    print("I am the function which needs some decoration to "
+          "remove my foul smell")
+
+a_function_requiring_decoration()
+#outputs: I am doing some boring work before executing a_func()
+#         I am the function which needs some decoration to remove my foul smell
+#         I am doing some boring work after executing a_func()
+
+#the @a_new_decorator is just a short way of saying:
+a_function_requiring_decoration = a_new_decorator(a_function_requiring_decoration)
+
+print(a_function_requiring_decoration.__name__)
+
+from functools import wraps
+
+def a_new_decorator(a_func):
+    @wraps(a_func)
+    def wrapTheFunction():
+        print("I am doing some boring work before executing a_func()")
+        a_func()
+        print("I am doing some boring work after executing a_func()")
+    return wrapTheFunction
+
+@a_new_decorator
+def a_function_requiring_decoration():
+    """Hey yo! Decorate me!"""
+    print("I am the function which needs some decoration to "
+          "remove my foul smell")
+
+print(a_function_requiring_decoration.__name__)
+# Output: a_function_requiring_decoration
---
+++ test.py
@@ -0,0 +1,332 @@
+# -*-coding:utf-8-*-
+def foo(x):
+    print "executing foo(%s)"%(x)
+
+class A(object):
+    print "test"
+    def foo(self,x):
+        print "executing foo(%s,%s)"%(self,x)
+
+    @classmethod
+    def class_foo(cls,x):
+        print "executing class_foo(%s,%s)"%(cls,x)
+
+    @staticmethod
+    def static_foo(x):
+        print "executing static_foo(%s)"%x
+
+a=A()
+a.foo(1)
+a.class_foo(1)
+a.static_foo(1)
+
+A.class_foo(2)
+A.static_foo(2)
+
+
+a = [1,2,3]
+b = {'a':1,'b':2,'c':3}
+c = True
+print type(a),type(b),type(c) # <type 'list'> <type 'dict'> <type 'bool'>
+print isinstance(a,list)  # True
+
+#iterable=dict(a, b)
+iterable = {'key': 95, 'Bob': 75, 'Tracy': 85}
+#for (key, value) in iterable:
+#    print key
+#    print value
+d = {value:  key for (key, value) in iterable.items()}
+print d
+
+L = [x*x for x in range(10)]
+g = (x*x for x in range(10))
+print L
+print g
+
+mcase = {'a': 10, 'b': 34, 'A': 7, 'Z': 3}
+
+mcase_frequency = {
+    k.lower(): mcase.get(k.lower(), 0) + mcase.get(k.upper(), 0)
+    for k in mcase.keys()
+}
+print mcase_frequency
+
+d={v: k for k, v in mcase.items()}
+print d
+
+squared = {x**2 for x in [1, 1, 2,8]}
+print(squared)
+
+
+def test_var_args(f_arg, *argv):
+    print("first normal arg:", f_arg)
+    for arg in argv:
+        print("another arg through *argv:", arg)
+
+test_var_args('yasoob', 'python', 'eggs', 'test')
+
+def greet_me(**kwargs):
+    for key, value in kwargs.items():
+        print("{0} == {1}".format(key, value))
+greet_me(name="zhaohsun")
+
+def test_args_kwargs(arg1, arg2, arg3):
+    print("arg1:", arg1)
+    print("arg2:", arg2)
+    print("arg3:", arg3)
+
+args = ("two", 3, 5)
+test_args_kwargs(*args)
+
+kwargs = {"arg3": 3, "arg2": "two", "arg1": 5}
+test_args_kwargs(**kwargs)
+
+
+def generator_function():
+    for i in range(10):
+        yield i
+
+for item in generator_function():
+    print(item)
+
+
+items = [1, 2, 3, 4, 5]
+squared = []
+for i in items:
+    squared.append(i**2)
+print squared
+
+items = [1, 2, 3, 4, 5]
+squared = list(map(lambda x: x**2, items))
+print squared
+
+
+def multiply(x):
+    return (x * x)
+
+
+def add(x):
+    return (x + x)
+
+
+funcs = [multiply, add]
+for i in range(5):
+    value = map(lambda x: x(i), funcs)
+    print(list(value))
+
+number_list = range(-5, 5)
+less_than_zero = filter(lambda x: x < 0, number_list)
+print(list(less_than_zero))
+# 译者注：上面print时，加了list转换，是为了python2/3的兼容性
+#        在python2中filter直接返回列表，但在python3中返回迭代器
+#        因此为了兼容python3, 需要list转换一下
+
+
+from functools import reduce
+product = reduce( (lambda x, y: x * y), [1, 2, 3, 4] )
+print product
+
+some_list = ['a', 'b', 'c', 'b', 'd', 'm', 'n', 'n']
+
+duplicates = []
+for value in some_list:
+    if some_list.count(value) > 1:
+        if value not in duplicates:
+            duplicates.append(value)
+
+print(duplicates)
+
+some_list = ['a', 'b', 'c', 'b', 'd', 'm', 'n', 'n']
+duplicates = set([x for x in some_list if some_list.count(x) > 1])
+print(duplicates)
+
+print set(some_list)
+
+
+valid = set(['yellow', 'red', 'blue', 'green', 'black'])
+input_set = set(['red', 'brown'])
+print(input_set.intersection(valid))
+
+valid = set(['yellow', 'red', 'blue', 'green', 'black'])
+input_set = set(['red', 'brown'])
+print(input_set.difference(valid))
+### 输出: set(['brown'])
+
+a_set = {'red', 'blue', 'green'}
+print(type(a_set))
+
+is_fat = True
+state = "fat" if is_fat else "not fat"
+print state
+
+fat = True
+fitness = ("skinny", "fat")[fat]
+print("Ali is", fitness)
+#输出: Ali is fat
+
+condition = True
+print(2 if condition else 1 / 0)
+#输出: 2
+
+# print((1 / 0, 2)[condition])
+#输出ZeroDivisionError异常
+
+
+# 装饰器
+print "装饰器"
+def hi(name="yasoob"):
+    return "hi " + name
+
+print(hi())
+# output: 'hi yasoob'
+
+# 我们甚至可以将一个函数赋值给一个变量，比如
+greet = hi
+# 我们这里没有在使用小括号，因为我们并不是在调用hi函数
+# 而是在将它放在greet变量里头。我们尝试运行下这个
+
+print(greet())
+# output: 'hi yasoob'
+
+# 如果我们删掉旧的hi函数，看看会发生什么！
+del hi
+#print(hi())
+#outputs: NameError
+
+print("after del, greet()"+greet())
+#outputs: 'hi yasoob'
+
+
+
+foo = ['hi']
+print(foo)
+# Output: ['hi']
+
+bar = foo
+bar += ['bye']
+print(foo)
+# Output: ['hi', 'bye']
+
+
+def add_to(num, target=[]):
+    target.append(num)
+    return target
+
+print add_to(1)
+# Output: [1]
+
+print add_to(2)
+# Output: [1, 2]
+
+print add_to(3)
+# Output: [1, 2, 3]
+
+
+def add_to2(num, target=[]):
+    target.append(num)
+    return target
+
+print add_to2(1)
+# Output: [1]
+
+print add_to2(2)
+# Output: [1,2]
+
+print add_to2(3)
+# Output: [1,2,3]
+
+
+from collections import defaultdict
+
+colours = (
+    ('Yasoob', 'Yellow'),
+    ('Ali', 'Blue'),
+    ('Arham', 'Green'),
+    ('Ali', 'Black'),
+    ('Yasoob', 'Red'),
+    ('Ahmed', 'Silver'),
+)
+
+favourite_colours = defaultdict(list)
+
+for name, colour in colours:
+    favourite_colours[name].append(colour)
+
+print(favourite_colours)
+
+import collections
+tree = lambda: collections.defaultdict(tree)
+some_dict = tree()
+some_dict['colours']['favourite'] = "yellow"
+print some_dict
+
+
+from collections import Counter
+
+colours = (
+    ('Yasoob', 'Yellow'),
+    ('Ali', 'Blue'),
+    ('Arham', 'Green'),
+    ('Ali', 'Black'),
+    ('Yasoob', 'Red'),
+    ('Ahmed', 'Silver'),
+)
+
+favs = Counter(name for name, colour in colours)
+fa_colour = Counter(colour for name, colour in colours)
+print(favs)
+print fa_colour
+
+
+
+from collections import namedtuple
+
+Animal = namedtuple('Animal', 'name age type')
+perry = Animal(name="perry", age=31, type="cat")
+
+print(perry)
+
+## 输出: Animal(name='perry', age=31, type='cat')
+
+print(perry.name)
+
+
+Animal = namedtuple('Animal', 'name age type')
+perry = Animal(name="Perry", age=31, type="cat")
+print(perry._asdict())
+
+
+from collections import namedtuple
+from enum import Enum
+
+class Species(Enum):
+    cat = 1
+    dog = 2
+    horse = 3
+    aardvark = 4
+    butterfly = 5
+    owl = 6
+    platypus = 7
+    dragon = 8
+    unicorn = 9
+    # 依次类推
+
+    # 但我们并不想关心同一物种的年龄，所以我们可以使用一个别名
+    kitten = 1  # (译者注：幼小的猫咪)
+    puppy = 2   # (译者注：幼小的狗狗)
+
+Animal = namedtuple('Animal', 'name age type')
+perry = Animal(name="Perry", age=31, type=Species.cat)
+drogon = Animal(name="Drogon", age=4, type=Species.dragon)
+tom = Animal(name="Tom", age=75, type=Species.cat)
+charlie = Animal(name="Charlie", age=2, type=Species.kitten)
+
+print charlie.type == tom.type
+
+
+for counter, value in enumerate(some_list):
+    print(counter, value)
+
+my_list = ['apple', 'banana', 'grapes', 'pear']
+counter_list = list(enumerate(my_list, 1))
+print(counter_list)
